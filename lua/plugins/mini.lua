fc530f56456f6c49c0ef8892bbbe4114dead3ea0
Arvin Dev <dev.arvin123@gmail.com>
2024-08-07 14:58:08 +1000

    added mini.icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,6 +18,7 @@ return { -- Collection of various small independent plugins/modules
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
+    require('mini.icons').setup()
     --  require('mini.animate').setup()
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
