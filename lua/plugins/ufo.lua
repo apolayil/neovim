93b9c8b7da29c01886cea680185f708dce5a889f
Arvin Dev <dev.arvin123@gmail.com>
2024-08-07 15:01:29 +1000

    idk something was broken, and now fixed

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,26 +1,28 @@
 return {
   'kevinhwang91/nvim-ufo',
   dependencies = 'kevinhwang91/promise-async',
-  config = function()
+  opt = function()
     -- Example configuration:
-    vim.o.foldcolumn = '0' -- '0' is not bad, that column on the side that tells you amount of folds.
-    vim.o.foldlevel = 99 -- leave all folds open
-    vim.o.foldlevelstart = 99 -- start with everything open
-    vim.o.foldenable = true
+    vim.o.foldcolumn = '1' -- '1' is the default value, showing the fold column on the side.
+    vim.o.foldlevel = 99 -- Leave all folds open.
+    vim.o.foldlevelstart = 99 -- Start with everything open.
+    vim.o.foldenable = true -- Enable folding.
 
-    -- Using ufo provider in nvim lsp as much as possible
+    -- Using ufo provider in nvim as much as possible
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.foldingRange = {
       dynamicRegistration = false,
       lineFoldingOnly = true,
     }
-    local language_servers = { 'clangd' } -- Add the language servers you need
+
+    local language_servers = require('lspconfig').util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
     for _, ls in ipairs(language_servers) do
       require('lspconfig')[ls].setup {
         capabilities = capabilities,
         -- you can add other fields for setting up lsp server in this table
       }
     end
+
     require('ufo').setup()
   end,
 }
